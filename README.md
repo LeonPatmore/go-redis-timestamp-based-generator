aba9fa5f3a6cab8d61154db755750b681f7a3e88
Leon Patmore <leon.patmore@bath.edu>
2022-12-24 22:42:50 +0000

    adding example.

--- README.md
+++ README.md
@@ -8,3 +8,17 @@
 ## Redis Version
 
 7
+
+## Atmoic
+
+- Redis is single threaded, so commands can not happen at the same time.
+
+## Consistency
+
+- A Redis cluster is not strongly consistent. This is because write acks from replicas are all
+  async.
+
+## Resources
+
+- https://redis.io/docs/manual/patterns/distributed-locks/
+- https://kafka.apache.org/documentation/#producerconfigs
--- cmd/example/example.go
+++ cmd/example/example.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 
 	"github.com/go-redis/redis/v9"
+	"github.com/go-redsync/redsync/v4"
+	"github.com/go-redsync/redsync/v4/redis/goredis/v8"
 )
 
 var ctx = context.Background()
@@ -15,8 +17,16 @@ var client = redis.NewClient(&redis.Options{
 	DB: 0,  // use default DB
 })
 
+var pool = goredis.NewPool(client)
+
+var rs = redsync.New(pool)
+
 func main() {
+	mutex := rs.NewMutex("cool-list")
+	mutex.LockContext(ctx)
 	client.LPush(ctx, "cool-list", "1")
 
 	fmt.Println(client.LRange(ctx, "cool-list", 0, -1))
+
+	mutex.UnlockContext(ctx)
 }
